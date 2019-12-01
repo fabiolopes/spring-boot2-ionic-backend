1f725ee1e40c17f6a3637305537a6b9f4511ac10
Fabio Bione <fabiolopesbione@hotmail.com>
2019-12-01 19:42:04 -0300

    criando repository e services

---
+++ src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
@@ -0,0 +1,18 @@
+package com.nelioalves.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nelioalves.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	public Categoria buscar(Integer id) {
+		return categoriaRepository.findById(id).get();
+	}
+}
