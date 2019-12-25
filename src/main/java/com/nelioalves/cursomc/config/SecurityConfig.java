dd68e2b4e32f3d6e1cd93a84932e76dbef9cfd22
Fabio Bione <fabiolopesbione@hotmail.com>
2019-12-24 23:22:19 -0300

    Autorizando endpoints para perfis específicos

--- src/main/java/com/nelioalves/cursomc/config/SecurityConfig.java
+++ src/main/java/com/nelioalves/cursomc/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -24,6 +25,7 @@ import com.nelioalves.cursomc.security.JWTUtil;
 
 @Configuration
 @EnableWebMvc
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
 	@Autowired
@@ -43,6 +45,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			"/clientes/**"
 	};
 	
+	private static final String[] PUBLIC_MATCHERS_POST = {
+			"/clientes/**"
+	};
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		if(Arrays.asList(env.getActiveProfiles()).contains("test")){
@@ -53,6 +59,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		http.authorizeRequests()
 		.antMatchers(PUBLIC_MATCHERS).permitAll()
 		.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+		.antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
 		.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
 		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
